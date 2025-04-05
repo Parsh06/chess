4bd73a02c54b08416ffd549c94030e820fea0c12
Parsh Jain <parshjain46@gmail.com>
2025-04-05 23:36:27 +0530

    fix deployment

--- chess-predictor/vite.config.js
+++ chess-predictor/vite.config.js
@@ -7,6 +7,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
   css: {
     postcss: {
       plugins: [
